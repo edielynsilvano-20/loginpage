09b3cf7328f9db4fd71917fe23da3a19084f3d09
edielynsilvano-sys <edielynsilvano@gmail.com>
2026-03-07 06:25:04 +0800

    added admin and user dashboards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Admin } from './dashboards/admin/admin';
+import { User } from './dashboards/user/user';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'admin', component: Admin },
+  { path: 'user', component: User },
+  { path: '', redirectTo: 'user', pathMatch: 'full' }
+];
